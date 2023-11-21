5918e9cf4ac1ca354d2632cb98c4a5d2d90ba184
Mayank <mayankbansal125@gmail.com>
2023-11-22 01:07:16 +0530

    Setup tailwind css in frontend

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import "./App.css";
+import "./index.css";
 import { CounterButton, NewTabLink } from "ui";
 
 function App() {
@@ -10,10 +10,8 @@ function App() {
         <span>Kitchen Sink</span>
       </h1>
       <CounterButton />
-      <p className="description">
-        Built With{" "}
-        <NewTabLink href="https://turbo.build/repo">Turborepo</NewTabLink> +{" "}
-        <NewTabLink href="https://vitejs.dev/">Vite</NewTabLink>
+      <p className="text-blue-500 font-bold">
+        Text
       </p>
     </div>
   );
