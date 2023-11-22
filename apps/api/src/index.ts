bf75ccac6824c63c3dedcd3cd1aac9504b070dfd
Mayank <mayankbansal125@gmail.com>
2023-11-22 23:57:49 +0530

    Set up environments in apps

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,11 @@
 import { createServer } from "./server";
 import { log } from "logger";
+require("dotenv").config();
 
 // eslint-disable-next-line turbo/no-undeclared-env-vars
 const port = process.env.PORT || 5001;
 const server = createServer();
 
 server.listen(port, () => {
-  log(`Api running on ${port}` + " http://locahost:"+port);
+  log(`Api running on ${port}` + " http://locahost:" + port + process.env.SECRET);
 });
--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import "./index.css";
 import { CounterButton, NewTabLink } from "ui";
-
+const backend = import.meta.env.VITE_SERVER;
 function App() {
   return (
     <div className="container">
@@ -11,7 +11,7 @@ function App() {
       </h1>
       <CounterButton />
       <p className="text-blue-500 font-bold">
-        Text
+        Backend: {backend}
       </p>
     </div>
   );
